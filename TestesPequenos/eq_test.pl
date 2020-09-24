1f007aa4446fe6abc1fddc98592869db08ecdff3
JoaoLBarbosa <joao.barbosa@fc.up.pt>
2020-09-24 12:21:58 +0100

    removing duplicates

--- TestesPequenos/eq_test.pl
+++
@@ -1,5 +0,0 @@
-%:- closure_flag(on).
-%:- list_flag(on).
-
-
-p(A):-(B=C,B=f(D),C=f(a)),A=D.
--- TestesPequenos/pq.pl
+++
@@ -1,9 +0,0 @@
-%:- closure_flag(on).
-%:- list_flag(on).
-%:- type(tnam(struct,[tvar(-3)]),[func(a,[]),func(f,[tvar(-3)])]).
-
-p(A):-(q(B),B=C),A=C.
-
-q(D):-(r(E),E=F),D=F.
-	
-r(G):-G=a;(r(H),H=I),G=f(I).
